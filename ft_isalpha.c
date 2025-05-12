7a7aad9e724a3939cbb5b3222c8db76ad3230dd5
Khaldoun Alhanawi <khaldouna@Khaldouns-MacBook-Air.local>
2025-05-12 22:36:17 +0200

    isalpha home

---
+++ ft_isalpha.c
@@ -0,0 +1,13 @@
+int ft_isalpha()
+{
+    int i;
+    
+    i = 0;
+    while (str[i])
+    {
+        if ((str[i] < 'a' && str[i] > 'z') || (str[i] < 'A' && str[i] > 'Z'))
+            return (0);
+        i++;
+    }
+    return (1);
+}
