3807e30e904a5b024ff7ce4cf23c6f1ce18aa118
Khaldoun Alhanawi <khaldouna@Khaldouns-MacBook-Air.local>
2025-05-13 10:54:34 +0200

    fixed c2

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,12 +1,12 @@
-int ft_isalpha()
+int ft_isalpha(char *str)
 {
     int i;
     
     i = 0;
     while (str[i])
     {
-        if ((str[i] < 'a' && str[i] > 'z') || (str[i] < 'A' && str[i] > 'Z'))
-            return (0);
+        if ((str[i] < 'a' && str[i] > 'Z') || (str[i] < 'A') || (str[i] > 'z')) 
+        	return (0);
         i++;
     }
     return (1);
