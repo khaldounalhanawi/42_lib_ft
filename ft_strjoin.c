d1878fbf6e86183d96d5a8a0cbb33bcbf90b5a57
Khaldoun Alhanawi <khaldouna@Khaldouns-MacBook-Air.local>
2025-05-19 15:42:29 +0200

    ft_strjoin home

---
+++ ft_strjoin.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+char *ft_strjoin(char const *s1, char const *s2)
+{	
+	int	len1;
+	int len2;
+	int	len_total;
+	char	*p;
+	char	*original;
+
+	len1 = 0;
+	len2 = 0;
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+	len_total = len1 + len2 + 1;
+	p = malloc (len_total * sizeof(char));
+	if (!p)
+		return (NULL);
+	original = p;
+	while (*s1)
+		*p++ = *s1++;
+	while (*s2)
+		*p++ = *s2++;
+	*p = '\0';
+	return (original);
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+	char s1[] = "Hey";
+	char s2[] = "there!";
+
+	printf("%s\n", ft_strjoin(s1, s2));
+
+
+
+	return (0);
+}*/
