6bcaee874d87cf90020a53bc9ef64315dcc1ae0f
Khaldoun Alhanawi <khaldouna@Khaldouns-MacBook-Air.local>
2025-05-19 16:07:06 +0200

    ft_substr home correct

--- ft_substr.c
+++ ft_substr.c
@@ -19,11 +19,19 @@ size_t	ft_strlcpy(char * restrict dst, const char * restrict src, size_t dstsize
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*p;
+	int	s_len;
+	int	s2_len;
 
+	s_len = 0;
+	while (s[s_len])
+		s_len++;
+	if (start >= s_len)
+		return (malloc(1));
+	s2_len = s_len - start;
 	p = malloc((len + 1) * sizeof(unsigned char));
 	if (!p)
 		return (NULL);
-	ft_strlcpy(p, &s[start], len + 1);
+	ft_strlcpy(p, &s[start], s2_len + 1);
 	return(p);
 }
 /*
@@ -33,8 +41,8 @@ int main()
 {
 
 	char s[] = "hello world!";
-	int pos = 98;
-	size_t len = 6;
+	int pos = 5;
+	size_t len = 50;
 
 	printf("%s\n", ft_substr(s,pos,len));
 }*/
