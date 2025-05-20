a0eb3517bc1a6294df40b75406aa29c445eeaecd
Khaldoun Alhanawi <khaldouna@Khaldouns-MacBook-Air.local>
2025-05-20 22:09:35 +0200

    ft_split improve

--- ft_split.c
+++ ft_split.c
@@ -1,63 +1,108 @@
 #include <stdlib.h>
 
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	num;
+
+	num = 0;
+	while (src[num])
+		num++;
+	if (dstsize)
+	{
+		while (--dstsize && *src)
+			*dst++ = *src++;
+		*dst = '\0';
+	}
+	return (num);
+}
+
+int	get_next(char const *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (*s != c && *s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+int freq_cal(char const *s, char c)
+{
+	int	freq;
+
+	freq = 0;
+	while (*s)
+	{
+		if (*s == c)
+			freq++;
+		s++;
+	}
+	return (freq);
+}
+
 char **ft_split(char const *s, char c)
 {
-	int		len;
 	int		freq;
 	int		local_len;
 	char **p;
 	char **origin;
 	
-	len = 0;
-	freq = 0;
-	while (s[len])
-	{
-		if (s[len] == c)
-			freq++;
-		len++;
-	}
-	if (!freq)
+	freq = freq_cal(s, c);
+	if (!freq )
 		return (NULL);
-	freq += 2;	
+	freq += 2;
 	p = malloc ((freq) * sizeof (char *));
+	if (!p)
+		return (NULL);
 	origin = p;
 	while (--freq)
 	{
-		// get size
 		local_len = get_next(s, c);
-		// malloc it
 		*p = malloc ((local_len + 1) * sizeof(char));
-		//strcpy
-		ft_strlcpy(*p, src, local_len); 
-		//*p = "a";
+		if (!(*p))
+			return (NULL);
+		ft_strlcpy(*p, s, local_len + 1); 
 		s += local_len + 1;
 		p++;
 	}
 	*p = NULL;
-	
-
     return (origin);
-
 }
 
 #include <stdio.h>
 
 int main ()
 {
-    char s[] = "hey there, how are you?";
+    char s[] = "hey there how are you";
     char c = ' ';
+
+    // char s[] = "hey there how are you";
+    // char c = 'e';
+
+    // char s[] = "  ";
+    // char c = 'e';
+
+    // char s[] = "";
+    // char c = 'e';
+
+    // char s[] = "hey there i am";
+    // char c = 'f';
 
     char **k = ft_split(s, c);
 
-    int i = 6;
-    while (i--)
-    {
-        
-        printf("%s",*(k));
-        k = k + 1;
-
-    }
-
+    printf("%p\n", k);
+    if (k)
+	{   
+		int i = 0;
+	    while (k[i])
+	    {
+	        printf("%s\n",k[i]);
+	        i++;
+	    }
+	}
     return (0);
 
 
