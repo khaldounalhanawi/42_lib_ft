16d98dfc9113ec67b3b81531c9a1c5c18c888cca
Khaldoun Alhanawi <khaldouna@Khaldouns-MacBook-Air.local>
2025-05-25 16:33:21 +0200

    substr ready

--- ft_substr.c
+++ ft_substr.c
@@ -28,23 +28,47 @@ static size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	return (num);
 }
 
-void	len_cal(int *s_len, int *s2_len, unsigned int start, 
-	char const *s, size_t len)
+static void	len_cal(unsigned int *s_len, unsigned int *s2_len,
+	unsigned int start, char const *s)
 {
 	while (s[*s_len])
 		*s_len = *s_len + 1;
 	*s2_len = *s_len - start;
-	if (len < (size_t)(*s2_len))
-		*s2_len = (int) len;
+}
+
+static char	*body(char const *s, unsigned int start, size_t len)
+{
+	char			*p;
+	unsigned int	s_len;
+	unsigned int	s2_len;
+
+	s_len = 0;
+	s2_len = 0;
+	len_cal(&s_len, &s2_len, start, s);
+	if (len < (size_t)(s2_len))
+		s2_len = (int) len;
+	if (start >= s_len)
+	{
+		p = malloc(1);
+		if (!p)
+			return (NULL);
+		p[0] = '\0';
+		return (p);
+	}
+	p = malloc((s2_len + 1) * sizeof(unsigned char));
+	if (!p)
+		return (NULL);
+	ft_strlcpy(p, &s[start], s2_len + 1);
+	return (p);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*empty;
 	char	*p;
-	int		s_len;
-	int		s2_len;
 
+	if (!s)
+		return (NULL);
 	if (*s == '\0' || len == 0)
 	{
 		empty = malloc(1);
@@ -53,15 +77,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		empty[0] = '\0';
 		return (empty);
 	}
-	s_len = 0;
-	s2_len = 0;
-	len_cal(&s_len, &s2_len, start, s, len);
-	if (start >= (unsigned int) s_len)
-		return (malloc(1));
-	p = malloc((s2_len + 1) * sizeof(unsigned char));
-	if (!p)
-		return (NULL);
-	ft_strlcpy(p, &s[start], s2_len + 1);
+	p = body (s, start, len);
 	return (p);
 }
 /*
