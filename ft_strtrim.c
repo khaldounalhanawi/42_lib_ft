96bbebca20347dc8f133aebac364b5e211d5c69e
Khaldoun Alhanawi <khaldouna@Khaldouns-MacBook-Air.local>
2025-05-29 09:22:14 +0200

    ft_strim final

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -13,92 +13,144 @@
 #include <stdlib.h>
 #include "libft.h"
 
-static int	str_len(char *s)
-{
-	int	i;
-
-	i = 0;
-	while (*s++)
-		i++;
-	return (i);
-}
-
-static int	isin(char a, char *b)
-{
-	while (*b)
-	{
-		if (*b == a)
-			return (1);
-		b++;
-	}
-	return (0);
-}
-
-static char	*sliders(char const *s1, char const *set)
-{
-	int		last;
-	char	*new;
-
-	while (isin(*s1, (char *)set))
-		s1++;
-	last = str_len ((char *) s1) - 1;
-	if (last < 0)
-	{
-		new = malloc ((str_len ((char *) s1) + 1) * sizeof (char));
-		if (!new)
-			return (NULL);
-		ft_strlcpy (new, s1, (str_len ((char *) s1)));
-		return (new);
-	}
-	while (isin(s1[last], (char *) set))
-		last --;
-	new = malloc ((last + 2) * sizeof(char));
-	if (!new)
-		return (NULL);
-	ft_strlcpy (new, s1, last + 2);
-	return (new);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*new;
+	int		start;
+	int		end;
+	int		final_len;
 
-	if (!s1)
+	if (!s1 || !set)
 		return (NULL);
-	if (*s1 == '\0')
-	{
-		new = malloc (1);
-		if (!new)
-			return (NULL);
-		*new = '\0';
-		return (new);
-	}
-	if (!set)
-	{
-		new = ft_strdup (s1);
-		return (new);
-	}
-	new = sliders(s1, set);
+	if (!*s1 || !*set)
+		return (ft_strdup(s1));
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (end >= start && ft_strchr(set, s1[end]))
+		end--;
+	final_len = end - start + 2;
+	new = malloc (final_len * sizeof (char));
+	if (!new)
+		return (NULL);
+	ft_strlcpy(new, &s1[start], final_len);
 	return (new);
 }
 /*
+
+
+
 #include <stdio.h>
 
 int main()
 {
-	//char s1[] = "Hgelzloa gUgfzok!";
-	//char s2[] = "zgak";
-	          // 012345678 9 
-	//char s1[] = " . abcd";
-	//char s2[] = "";
-	
-	char s1[] = "aacacabaaacccb";
-	char s2[] = "abc";
+	int i = 0;
 
-	//char s1[] = "  \t \t \n   \n\n\n\t";
-	//char s2[] = " \n\t";
-	// ft_strtrim(s1, s2);
-	printf(">%s<\n", ft_strtrim(s1, s2));
+	char *t[][2] = 
+	{
+        {"", ""},
+        {"abc", ""},
+        {"", "abc"},
+        {"abc", "abc"},
+        {"ab123", "ab"},
+        {"123ab", "ab"},
+        {"ab123ab", "ab"},
+        {"ab123ab", "b"},
+        {"ab123ab", "a"},
+        {"ab123ab", "f"},
+        {"123ab12", "ab"},
+        {"abab123abab", "ab"},
+        {"ab123ab", "ff"},
+        {"ab123ab", "fa"},
+        {"a12ba3ab", "abc"},
+        {"ab123ab", "fabcdefghijk"},
+        {"Hgelzloa gUgfzok!", "zgak"},
+        {"", ""},
+        {"aacacabaaacccb", "abc"},
+        {"  \t \t \n   \n\n\n\t", " \n\t"},
+        {"   \t  \n\n \t\t  \n\n\nHello \t  Please\n 
+        Trim me !\n   \n \n \t\t\n  "," \n\t"}
+    };
+
+   	int ss = sizeof(t) / sizeof(t[0]);
+   	// printf("%lu\n", sizeof(t[0]));
+   	while (i < ss)
+   	{
+		printf("my >%s<\n", ft_strtrim(t[i][0], t[i][1]));
+		printf("pd >%s<\n", ft_strtrim2(t[i][0], t[i][1]));
+		printf("\n");
+   		i++;
+   	}
+
+
+	// char s1[] = "Hgelzloa gUgfzok!";
+	// char s2[] = "zgak";
+	          // 012345678 9 
+	// char s1[] = "";
+	// char s2[] = "";
+
+	
+	// char s1[] = "aacacabaaacccb";
+	// char s2[] = "abc";
+
+	// char s1[] = "  \t \t \n   \n\n\n\t";
+	// char s2[] = " \n\t";
+
+
+// char s1[] = "";
+// char s2[] = "";
+
+// char s1[] = "abc";
+// char s2[] = "";
+
+// char s1[] = "";
+// char s2[] = "abc";
+
+// char s1[] = "abc";
+// char s2[] = "abc";
+
+// char s1[] = "abc";
+// char s2[] = "afg";
+
+// char s1[] = "ab123";
+// char s2[] = "ab";
+
+// char s1[] = "123ab";
+// char s2[] = "ab";
+
+// char s1[] = "ab123ab";
+// char s2[] = "ab";
+
+// char s1[] = "ab123ab";
+// char s2[] = "b";
+
+// char s1[] = "ab123ab";
+// char s2[] = "a";
+
+// char s1[] = "ab123ab";
+// char s2[] = "f";
+
+// char s1[] = "123ab12";
+// char s2[] = "ab";
+
+// char s1[] = "abab123abab";
+// char s2[] = "ab";
+
+// char s1[] = "ab123ab";
+// char s2[] = "ff";
+
+// char s1[] = "ab123ab";
+// char s2[] = "fa";
+
+// char s1[] = "a12ba3ab";
+// char s2[] = "abc";
+
+// char s1[] = "ab123ab";
+// char s2[] = "fabcdefghijk";
+
+
 
 	return (0);
-}*/
+}
+*/
